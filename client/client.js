c19ffce473d85a01df71221b6ad0df9b9bc7cad6
Fred Magege <fred@cognitivesystems.co.za>
2016-11-23 13:40:36 +0200

    completed tutorial lesson

--- client/client.js
+++ client/client.js
@@ -1,1 +1,8 @@
+import React from 'react'
+import {render} from 'react-dom' //similar to: var render = require('react-dom').render;
+import App from '../components/App'
 
+render(
+  // define encompassing component and DOM element to mount
+  <App/>, document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,33 @@
+module.exports = {
+  entry: ['./client/client.js'],
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
 
+        // skip any files outside of components
+        //include : [
+          //path.resolve(__dirname, 'components')
+        //],
+
+        // only process .js and/or .jsx
+        test: /\.jsx?$/,
+
+        // exclude dir node_modules from processing
+        exclude: /node_modules/,
+
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+
+  // ONLY USE IN DEV ENV !!!
+  devtool: 'inline-source-map'
+}
